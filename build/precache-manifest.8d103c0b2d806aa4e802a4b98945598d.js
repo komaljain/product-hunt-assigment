b6f546490ce21e97666bde54255aca7bd7e2570e
Komal <komalj@ridlr.in>
2020-01-29 01:06:28 +0530

    build prod

---
+++ build/precache-manifest.8d103c0b2d806aa4e802a4b98945598d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe00dc05a75974a82e5bceadaa8c7517",
+    "url": "/komaljain/product-hunt-assigment/index.html"
+  },
+  {
+    "revision": "f995d3216f072f7fced2",
+    "url": "/komaljain/product-hunt-assigment/static/css/2.c4d1b98b.chunk.css"
+  },
+  {
+    "revision": "f5bd10e82094d415b6eb",
+    "url": "/komaljain/product-hunt-assigment/static/css/main.d72c3d38.chunk.css"
+  },
+  {
+    "revision": "f995d3216f072f7fced2",
+    "url": "/komaljain/product-hunt-assigment/static/js/2.8e388bdd.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/komaljain/product-hunt-assigment/static/js/2.8e388bdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5bd10e82094d415b6eb",
+    "url": "/komaljain/product-hunt-assigment/static/js/main.8d7c9f76.chunk.js"
+  },
+  {
+    "revision": "5c6b35d1ddefbbb359e0",
+    "url": "/komaljain/product-hunt-assigment/static/js/runtime-main.8c4ed3f6.js"
+  }
+]);
