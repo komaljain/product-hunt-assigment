5fae732357fffc276c7ecdc7b669f82ee682bd72
Komal <komalj@ridlr.in>
2020-01-28 13:38:15 +0530

    added index.html component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
         return (
             <BrowserRouter>
                 <Switch>
+                    <Route path='/index.html' component={HomePage}></Route>
                     <Route path='/homepage' component={HomePage}></Route>
                     <Route path='/myupvotes' component={MyUpVotes}></Route>
                 </Switch>
